c26201672ea64c8741901f45e086047854f09752
leesaewa <saewa0424@gmail.com>
2022-12-19 00:30:55 +0900

    221219:update

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ BTS Todo List made with VanillaJS
 
 ## update log
 
+#### 221219
+
+- `css`=>`scss` 교체
+- 모바일 레이아웃 변경
+
 #### 221218
 
 - `css`=>`scss` 교체
--- js/background.js
+++ js/background.js
@@ -1,13 +1,19 @@
-let images = ["bts01.jpg", "bts02.jpg", "bts03.jpg", "bts04.jpg", "bts05.jpg", "bts06.jpg", "bts07.jpg"];
+let images = [
+  "bts01.jpg",
+  "bts02.jpg",
+  "bts03.jpg",
+  "bts04.jpg",
+  "bts05.jpg",
+  "bts06.jpg",
+  "bts07.jpg",
+];
 
-window.onload = function() {
-    slideImg();
-    setInterval("slideImg()", 5000);
-}
+window.onload = function () {
+  slideImg();
+  setInterval("slideImg()", 5000);
+};
 
 function slideImg() {
-    let chosenImage = images[Math.floor(Math.random() * images.length)];
-    document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+  let chosenImage = images[Math.floor(Math.random() * images.length)];
+  document.body.style.backgroundImage = `url('img/${chosenImage}')`;
 }
-
-//document.body.appendChild(chosenImage);
